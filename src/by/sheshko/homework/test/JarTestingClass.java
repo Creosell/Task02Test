3a2c345c8777cbed186b5acdd68485035e3a64d7
Creosell <pavel.sheshko.bsuir@gmail.com>
2022-02-26 16:06:27 +0300

    Default

--- src/by/sheshko/homework/test/JarTestingClass.java
+++ src/by/sheshko/homework/test/JarTestingClass.java
@@ -2,8 +2,12 @@ package by.sheshko.homework.test;
 
 import by.sheshko.homework.task02.MathCalculator;
 
+/**
+ * Класс JarTestingClass предназначен для проверки работоспособности
+ * подклчюенной библиотеки Task02.jar.
+ */
 public class JarTestingClass {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         MathCalculator calculator = new MathCalculator();
 
         System.out.println(calculator.calculateExpression("(2*3)+(10/2)-1"));
